0a781c87f6571c34123ad68d976b211ebab2c3d6
zmcx16 <zmcx16@gmail.com>
2020-02-09 22:10:42 +0800

    Init repo with gatsby

---
+++ src/components/loginBox.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Button from '@material-ui/core/Button';
+
+class LoginBox extends Component {
+  render() {
+    return (
+      <div>{'Hello world!'}
+        <Button>Submit</Button>
+      </div>
+    );
+  }
+}
+
+export default LoginBox
