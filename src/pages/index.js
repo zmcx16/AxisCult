17ffb52750ee9c772ac21658839ef36d3bbe8341
zmcx16 <zmcx16@gmail.com>
2020-03-05 01:40:26 +0800

    add react-intl for i18n

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
+import { IntlProvider } from "react-intl"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
@@ -7,18 +8,24 @@ import SEO from "../components/seo"
 
 import LoginBox from "../components/loginBox"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-	<LoginBox/>
-  </Layout>
-)
+const IndexPage = () => {
+  const locale = navigator.language
+
+  return (
+    <IntlProvider locale={locale} key={locale} defaultLocale="en">
+      <Layout>
+        <SEO title="Home" />
+        <h1>Hi people</h1>
+        <p>Welcome to your new Gatsby site.</p>
+        <p>Now go build something great.</p>
+        <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+          <Image />
+        </div>
+        <Link to="/page-2/">Go to page 2</Link>
+        <LoginBox/>
+      </Layout>
+    </IntlProvider>
+  )
+}
 
 export default IndexPage
